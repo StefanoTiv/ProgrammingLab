0e954b62424b0a0f721f0dc697746fee78bd06da
Stefano Tivan <94189961+StefanoTiv@users.noreply.github.com>
2021-11-23 16:29:02 +0000

    Ho fatto la funzione somma

---
+++ Primo_esercizio.py
@@ -0,0 +1,14 @@
+lista_numeri = [4, 56, 30, 12]
+
+def Risultato(lista_numeri):
+    somma = 0 
+    for item in lista_numeri:
+        somma = somma + item 
+    print('La somma è {}' .format(somma))
+
+somma = Risultato(lista_numeri)
+
+
+
+
+    
---
+++ shampoo_sales.py
@@ -0,0 +1,12 @@
+values[]
+
+def shampoo_s(values):
+    my_file = open('shampoo_sales', 'r')
+    for line in my_file:
+        elements = line.split(',')
+        if elements[0] != 'Date':
+            date = elements[0]
+            value = elements[1]
+            values.append(float(value))
+
+  
